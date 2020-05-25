97739c10b4da65e96937750cb7b5e3f65b2a2821
SATYANARAYANA UPPADA <47477333+usatya99@users.noreply.github.com>
2020-05-25 17:59:00 +0530

    Update CODEVITA_2019_Coin_Distribution.py

--- MATH/Numbers/CODEVITA_2019_Coin_Distribution.py
+++ MATH/Numbers/CODEVITA_2019_Coin_Distribution.py
@@ -1,24 +1,16 @@
-first_ten = [[0,0,1],[0,0,2],[0,1,1],[0,1,2],[0,2,1],[0,2,2],[0,3,1],[0,3,2],[1,1,2],[1,2,1]] 
-def print_config(config):
-    total = config[0]+config[1]+config[2];
-    print(str(total) + " " + str(config[0])+" "+str(config[1])+" "+str(config[2]))
-n=int(input())
-if(n<10):
-    print_config(first_ten[n-1]);
+value=int(input())
+#count the number of five rupee coin
+five=int((value-4)/5)
+#count the nimber of onr rupee coin 
+if (value-five*5)%2==0: 
+    one=2
 else:
-    config=[0]*3
-    counter_five = 0;
-    remainder = 0;
-    if(n%10 == 0):
-        counter_five = (n-10)//5
-        config = first_ten[9]
-        config[0]+= counter_five
-    elif(n%10 < 5):
-        counter_five = (n-(n%10)-5)//5
-        config = first_ten[n%10+5-1]
-        config[0]+= counter_five
-    elif(n%10>=5):
-        counter_five = (n-(n%10))//5
-        config = first_ten[n%10-1]
-        config[0]+= counter_five
-    print_config(config)
+    one=1    
+#counnt the number of two rupee coin
+two=int((value-5*five-one)/2)
+#print the required number
+print(five+one+two,five,two,one)
+
+
+
+
